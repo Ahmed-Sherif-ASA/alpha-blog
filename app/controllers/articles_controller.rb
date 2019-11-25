69e0c3926a0b6822d541c4ec9a3f92c828f97920
Ahmed-Sherif-ASA <ahmedsherifakl.asa@gmail.com>
2019-11-25 04:15:15 +0200

    ,Add edit, update, show and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,38 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = 'Article was created successfully'
+      flash[:notice] = "Article was created successfully"
       redirect_to article_path(@article)
     else
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was created updated"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
 
   def show
     @article = Article.find(params[:id])
