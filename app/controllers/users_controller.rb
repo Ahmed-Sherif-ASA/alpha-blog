7e68bf341436dd82f6b4436ef9ca4f9788cdeec3
Ahmed-Sherif-ASA <ahmedsherifakl.asa@gmail.com>
2019-12-06 14:40:16 +0200

    Add pagination to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save 
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to articles_path
     else
@@ -28,12 +33,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
-  def index
-    @users = User.all
-  end
-
+  
   private 
   def user_params
     params.require(:user).permit(:username, :email, :password)
